355ac39f982da4a33b4125969f9ab5bd1ce7f12b
Alex Sadykov <92750261+stopperstopper@users.noreply.github.com>
2022-01-29 19:05:43 +0500

    Update ConfProperties.java
    v1.1

--- src/test/java/tests/ConfProperties.java
+++ src/test/java/tests/ConfProperties.java
@@ -31,6 +31,6 @@ public class ConfProperties {
                 } } }
 
     // метод для возврата строки со значением из файла с настройками 
-
+ 
     public static String getProperty(String key) {
         return PROPERTIES.getProperty(key); } }
